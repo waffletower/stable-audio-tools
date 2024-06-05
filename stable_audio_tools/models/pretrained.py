c3fb7181705a1e25f1cf2736c25dcc991623a596
Christopher Penrose <waffletower@users.noreply.github.com>
2024-06-05 14:13:30 -0700

    plumb cache_dir configuration in pretrained.py

--- stable_audio_tools/models/pretrained.py
+++ stable_audio_tools/models/pretrained.py
@@ -5,9 +5,12 @@ from .utils import load_ckpt_state_dict
 
 from huggingface_hub import hf_hub_download
 
-def get_pretrained_model(name: str):
-    
-    model_config_path = hf_hub_download(name, filename="model_config.json", repo_type='model')
+def get_pretrained_model(name: str, cache_dir=None):
+
+    model_config_path = hf_hub_download(name,
+                                        filename="model_config.json",
+                                        repo_type='model',
+                                        cache_dir=cache_dir)
 
     with open(model_config_path) as f:
         model_config = json.load(f)
@@ -16,9 +19,15 @@ def get_pretrained_model(name: str):
 
     # Try to download the model.safetensors file first, if it doesn't exist, download the model.ckpt file
     try:
-        model_ckpt_path = hf_hub_download(name, filename="model.safetensors", repo_type='model')
+        model_ckpt_path = hf_hub_download(name,
+                                          filename="model.safetensors",
+                                          repo_type='model',
+                                          cache_dir=cache_dir)
     except Exception as e:
-        model_ckpt_path = hf_hub_download(name, filename="model.ckpt", repo_type='model')
+        model_ckpt_path = hf_hub_download(name,
+                                          filename="model.ckpt",
+                                          repo_type='model',
+                                          cache_dir=cache_dir)
 
     model.load_state_dict(load_ckpt_state_dict(model_ckpt_path))
 
